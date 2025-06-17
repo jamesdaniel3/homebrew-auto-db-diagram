88f30a3071adddc846a8a2fc7fb9926af0877fd9
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-06-17 13:35:17 -0400

    updated sha value

--- Formula/db-diagram.rb
+++ Formula/db-diagram.rb
@@ -2,7 +2,7 @@ class DbDiagram < Formula
     desc "A command-line tool that allows for connecting to and automatically diagramming SQL database instances"
     homepage "https://github.com/jamesdaniel3/auto-db-diagrammer"
     url "https://github.com/jamesdaniel3/auto-db-diagrammer/archive/v1.0.0.tar.gz"
-    sha256 "..."
+    sha256 "4af9bb8cd63b5e9226c133301a74406ba9369c1a1b76292fbf33d35c2def56c4"
     license "MIT"
     
     depends_on "jq"
