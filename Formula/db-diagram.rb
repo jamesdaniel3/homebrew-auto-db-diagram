9b63f40cf389e9a1f5f93289a4e7bba02ec26eb4
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-06-18 17:18:10 -0400

    added man page installation

--- Formula/db-diagram.rb
+++ Formula/db-diagram.rb
@@ -1,20 +1,25 @@
 class DbDiagram < Formula
-    desc "A command-line tool that allows for connecting to and automatically diagramming SQL database instances"
-    homepage "https://github.com/jamesdaniel3/auto-db-diagram"
-    url "https://github.com/jamesdaniel3/auto-db-diagram/archive/v2.0.0.tar.gz"
-    sha256 "116e3102bc6813c412b01cf602cc90594c3c436943c8c17db665896de2afbb63"
-    license "MIT"
-    
-    depends_on "jq"
-    depends_on "postgresql" 
-    depends_on "graphviz"
-    depends_on "python@3.11"
-    
-    def install
-      libexec.install Dir["*"]
-      (bin/"db-diagram").write <<~EOS
-        #!/bin/bash
-        exec "#{libexec}/main.sh" "$@"
-      EOS
+  desc "A command-line tool that allows for connecting to and automatically diagramming SQL database instances"
+  homepage "https://github.com/jamesdaniel3/auto-db-diagram"
+  url "https://github.com/jamesdaniel3/auto-db-diagram/archive/v2.0.0.tar.gz"
+  sha256 "116e3102bc6813c412b01cf602cc90594c3c436943c8c17db665896de2afbb63"
+  license "MIT"
+  
+  depends_on "jq"
+  depends_on "postgresql" 
+  depends_on "graphviz"
+  depends_on "python@3.11"
+  
+  def install
+    libexec.install Dir["*"]
+    (bin/"db-diagram").write <<~EOS
+      #!/bin/bash
+      exec "#{libexec}/main.sh" "$@"
+    EOS
+
+    # install manpage 
+    if File.exist?("#{buildpath}/db-diagram.1")
+      man1.install "#{buildpath}/db-diagram.1"
     end
+  end
 end
