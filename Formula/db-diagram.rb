56c2fa4e274b8ae9b4cef78df4bbfd8cfed2d8a4
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-06-20 12:14:37 -0400

    updated the formula for the new release

--- Formula/db-diagram.rb
+++ Formula/db-diagram.rb
@@ -1,18 +1,19 @@
 class DbDiagram < Formula
   desc "A command-line tool that allows for connecting to and automatically diagramming SQL database instances"
   homepage "https://github.com/jamesdaniel3/auto-db-diagram"
-  url "https://github.com/jamesdaniel3/auto-db-diagram/archive/v2.1.0.tar.gz"
-  sha256 "0de591b6e47a02afb85ed3ba813466b616ba4dc0c946becb6cac608e8f7de545"
+  url "https://github.com/jamesdaniel3/auto-db-diagram/archive/v2.2.0.tar.gz"
+  sha256 "4915aacd8752704541a684b831a0a200fd446328b729006ba7a6f246e720c32b"
   license "MIT"
   
   depends_on "jq"
   depends_on "postgresql" 
+  depends_on "mysql" 
   depends_on "graphviz"
   depends_on "python@3.11"
   
   def install
     # Install everything except the man page and unnecessary files to libexec
-    libexec.install Dir["*"] - ["db-diagram.1", "ERD.png", "README.md", ".github", "tests"]
+    libexec.install Dir["*"] - ["db-diagram.1", "ERD.png", ".gitignore", "README.md", ".github", "tests"]
     
     (bin/"db-diagram").write <<~EOS
       #!/bin/bash
